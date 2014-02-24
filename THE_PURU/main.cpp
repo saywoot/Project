116308daac5455332ac14cc869ad235459d14690
saywoot <w00tponey@gmail.com>
2014-02-24 14:33:13 +0100

    Initial Commit

---
+++ THE_PURU/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  THE_PURU
+//
+//  Created by sayw00t on 24/02/2014.
+//  Copyright (c) 2014 sayw00t. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
